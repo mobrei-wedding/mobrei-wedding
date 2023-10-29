c26a118a5806c366764b228bc2e941e31465e386
bomb <garnix816@gmail.com>
2023-10-29 17:28:19 +0100

    remove comments

--- js/send_emails.js
+++ js/send_emails.js
@@ -1,19 +1,3 @@
-// function sendEmail() {
-    
-// 	Email.send({
-// 	Host: "smtp.elasticemail.com",
-//     PORT: "2525",
-// 	Username : "mobrei.wedding@gmail.com",
-// 	Password : "7769F2AADB8EE88FC35B999978B83D094B26",
-// 	To : 'mobrei.wedding@gmail.com',
-// 	From : "mobrei.wedding@gmail.com",
-// 	Subject : "Test",
-// 	Body : "What huh?",
-// 	}).then(
-// 		message => console.log("message:", message)
-// 	);
-// }
-
 function sendEmail(attachment, receiver) {
 	// https://support.google.com/a/answer/2956491?sjid=14902451573699173167-EU
 	var subject = "【茂靈2539婚禮茶會】 " + receiver + "#購買明細";
