d4504fb38cefb6c402bbeb84ca1306fd11444094
bomb <garnix816@gmail.com>
2023-10-29 07:22:56 +0100

    email update

--- js/send_emails.js
+++ js/send_emails.js
@@ -27,8 +27,9 @@ function sendEmail(attachment, receiver) {
 		From : "mobrei.wedding@gmail.com",
 		Bcc : ["mobrei.wedding@gmail.com"],
 		Subject : subject,
-		Body : `賓客您好，<br>茂靈2539婚禮茶會的籌備組已收到您的報名，附件為您的報名購買明細，主辦方與您各持有一份，此信為系統自動寄信，不需回覆。<br><br>
-		若成功報名且茶會確定舉行，主辦方將寄送匯款通知信，再請您留意信箱，感謝您。<br><br>
+		Body : `賓客您好，<br>茂靈2539婚禮茶會的籌備組已收到您的報名，附件為您的報名購買明細，主辦方與您各持有一份，此為系統自動寄信，不需回覆。<br><br>
+		若成功報名且茶會確定舉行，主辦方將寄送<b>匯款通知信</b>，再請您留意信箱，感謝。<br>
+		若當天有意願擔任小精靈的來賓也請留意信箱的社群加入通知，再次感謝您！
 		<br>
 		如有需要進行報名細項的修正，請聯絡籌備組官方信箱: <strong>mobrei.wedding@gmail.com</strong><br><br>
 		《One And Only One》 茂靈婚禮茶會籌備組<br>`,
